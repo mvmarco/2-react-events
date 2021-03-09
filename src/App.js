9ea87bf52d8bed5e1a39c18e33bca3215f1de321
marco corapi <marco.corapi@outlook.it>
2021-03-09 11:07:39 +0100

    events initiated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+// #################################
+// EVENTS
+// #################################
+/* 
+  React event listener are handled in a simpler way.
+  instead of doing the querySelector for a button for instance, and then doing
+  .addEventListener('click', handleClick). In React we do as follow:
+
+  <button onClick={.......here we add the function......}>Click</button>
+
+*/
+
 import './App.css';
 import TweetList from './components/TweetList.js'
 import CreateTweet from './components/CreateTweet.js'
 function App() {
+  const sayHelloHandler = (user) => console.log(`hello ${user}`);
   return (
     <div>
       <h1>Hello React</h1>
+      <button onClick={sayHelloHandler('Marco')}>Click</button>
       <CreateTweet />
       <TweetList />
     </div>
